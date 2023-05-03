f94a7cc31720f53e38b75fcf6fc90bf4faaa8ada
Dylan Morrison <dylanjmorrison30@gmail.com>
2023-05-03 17:24:21 +0100

    updated .gitignore for rust files

--- build_tool/target/debug/deps/build_tool-f3c03a69723f4abb.d
+++
@@ -1,5 +0,0 @@
-/Users/dylan/Code/web/extensions/distraction-free-reddit/build_tool/target/debug/deps/build_tool-f3c03a69723f4abb.rmeta: src/main.rs
-
-/Users/dylan/Code/web/extensions/distraction-free-reddit/build_tool/target/debug/deps/build_tool-f3c03a69723f4abb.d: src/main.rs
-
-src/main.rs:

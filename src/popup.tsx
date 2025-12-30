5be7e1e1b12305f16c1b35b3a073fc9746ca2697
Dylan Morrison <dylanjmorrison30@gmail.com>
2025-12-30 18:20:21 +0000

    Remove comment

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./views/Popup/App";
 import "./index.css";
 
-// React 18+ uses createRoot instead of render
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
